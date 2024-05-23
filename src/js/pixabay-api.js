04abf672e1d0290bc05175e61963814b3c40daaf
Elijah-Vakulenko <154387383+Elijah-Vakulenko@users.noreply.github.com>
2024-05-23 23:19:41 +0100

    css edit

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,16 +1,28 @@
 function getImages() {
     const BASE_URL = 'https://pixabay.com/api/';
-    const END_POINT = '';
     const params = new URLSearchParams({
         key: '44023178-1b17cf85b995cf2d6fd44a474',
         q,
         image_type: 'photo',
         orientation: 'horizontal',
         safesearch: true,
+          per_page: 20,
     });
 
-    const url = `${BASE_URL}${END_POINT}?${params}`;
+    const url = `${BASE_URL}?${params}`;
 
-    return fetch(url).then(response => response.json());
-}
+    return fetch(url).then(response => response.json())
+    .then(data => {
+      if (data.hits.length === 0) {
+        iziToast.error({
+          title: 'Error',
+          message:
+            'Sorry, there are no images matching your search query. Please try again!',
+          position: 'topRight',
+        });
+      }
+      return data.hits;
+    });
+};
+
 
