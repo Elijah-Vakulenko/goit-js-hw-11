a2ecf7f31799ec5fee3b713c4f2ac41e38b56a04
Elijah-Vakulenko <154387383+Elijah-Vakulenko@users.noreply.github.com>
2024-05-23 22:25:16 +0100

    Update pixabay-api.js

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -2,7 +2,7 @@ function getImages() {
     const BASE_URL = 'https://pixabay.com/api/';
     const END_POINT = '';
     const params = new URLSearchParams({
-        key: 44023178-1b17cf85b995cf2d6fd44a474,
+        key: '44023178-1b17cf85b995cf2d6fd44a474',
         q,
         image_type: 'photo',
         orientation: 'horizontal',
