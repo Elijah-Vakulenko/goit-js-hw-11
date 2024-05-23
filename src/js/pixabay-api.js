9da9903835f34fc2e5d8baac3943d103c7a2a35c
Elijah-Vakulenko <154387383+Elijah-Vakulenko@users.noreply.github.com>
2024-05-23 22:19:12 +0100

    loader added

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,2 +1,16 @@
-const BASE_URL = 'https://pixabay.com/api/';
+function getImages() {
+    const BASE_URL = 'https://pixabay.com/api/';
+    const END_POINT = '';
+    const params = new URLSearchParams({
+        key: 44023178-1b17cf85b995cf2d6fd44a474,
+        q,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+    });
+
+    const url = `${BASE_URL}${END_POINT}?${params}`;
+
+    return fetch(url).then(response => response.json());
+}
 
