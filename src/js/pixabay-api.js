1d43c7cf3d84674349993a54f5e4912c777700f5
Elijah-Vakulenko <154387383+Elijah-Vakulenko@users.noreply.github.com>
2024-05-23 23:39:12 +0100

    loaded class added

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -12,17 +12,7 @@ function getImages() {
     const url = `${BASE_URL}?${params}`;
 
     return fetch(url).then(response => response.json())
-    .then(data => {
-      if (data.hits.length === 0) {
-        iziToast.error({
-          title: 'Error',
-          message:
-            'Sorry, there are no images matching your search query. Please try again!',
-          position: 'topRight',
-        });
-      }
-      return data.hits;
-    });
+    
 };
 
 
