a5b526477d6996e427312d1fb3ad77f573f7fc80
Elijah-Vakulenko <154387383+Elijah-Vakulenko@users.noreply.github.com>
2024-05-25 13:25:00 +0100

    fetch, render, styles addede

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,18 +1,35 @@
-function getImages() {
+export function getImages(query) {
     const BASE_URL = 'https://pixabay.com/api/';
     const params = new URLSearchParams({
-        key: '44023178-1b17cf85b995cf2d6fd44a474',
-        q,
+        key: '44023178-1b17cf85b995cf2d6fd44a474', // ← Персональний ключ
+        q: query, //← Введений нами запит
         image_type: 'photo',
         orientation: 'horizontal',
         safesearch: true,
-          per_page: 20,
+        page: 1,
+        per_page: 20,
     });
 
     const url = `${BASE_URL}?${params}`;
 
-    return fetch(url).then(response => response.json())
-    
+    return fetch(url).then(response => {
+        if (!response.ok) {
+            throw new Error(response.statusText)
+        }
+        return response.json();
+    })
+        .then(data => {
+        if (data.totalHits === 0) {
+                iziToast.error({
+                    title: 'Error!',
+                    message: 'Sorry, there are no images matching your search query. Please try again!',
+                    position: 'topRight',
+                });
+                throw new Error('No images found');
+            }
+            return data;
+        })
+        .catch(error => {
+            console.error("Error fetching images:", error);
+        });
 };
-
-
--- src/main.js
+++ src/main.js
@@ -4,22 +4,43 @@ import "izitoast/dist/css/iziToast.min.css";
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 
-import { fetchImages } from './js/pixabay-api.js';
-import { galleryRender } from './js/render-functions.js';
+import { getImages } from './js/pixabay-api.js'; //← функція фетчу зображень з серверу
+import { galleryRender} from './js/render-functions.js'; //← імпорт функції рендеру
+
+//===============================================================================
+// імпорт DOM елементів
 
 const refs = {
     form: document.querySelector('.search-form'),
-    input: document.querySelector('input'),
-    searchBtn: document.querySelector('button'),
+    input:  document.querySelector('input'),
     gallery: document.querySelector('.gallery'),
-    loadMoreBtn: document.querySelector('.load-more'),
+    loader: document.querySelector('.loader'),
 }
 
-function imagesSearch(event) {
+//===============================================================================
+// Налаштовуємо SimpleLightbox
+
+function initilizeLightbox() {
+    const lightbox = new SimpleLightbox('.gallery a', {
+    captionDelay: 250,
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    });
+    
+     lightbox.refresh();
+}
+
+//===============================================================================
+// Налаштовуємо слухач подій та визначаємо, що буде відбуватися при запиті
+
+refs.form.addEventListener('submit', event => {
     event.preventDefault();
-}
+    const query = event.target.elements.query.value.trim();
 
-
-
-
-refs.form.addEventListener('submit', imagesSearch);
+    getImages(query).then(data => {
+        const markup = galleryRender(data.hits)
+        refs.gallery.innerHTML = markup;
+        initilizeLightbox()
+        refs.loader.classList.add('is-hidden');
+    });
+});
